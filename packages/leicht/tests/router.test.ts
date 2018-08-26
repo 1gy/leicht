115a16c094857d0ea94548bb46dd10910daad70e
1gy <tosh@tosh.local>
2018-08-26 23:38:26 +0900

    Add namespace test

--- packages/leicht/tests/router.test.ts
+++ packages/leicht/tests/router.test.ts
@@ -60,4 +60,52 @@ describe('router', () => {
             server.close();
         }
     });
+
+    it('namespace', async () => {
+        const path1 = get('/path1')(async (req, res) => {
+            return 'get path1';
+        });
+        const path2 = post('/path2')(async (req, res) => {
+            return 'post path2';
+        });
+        const nestNotFound: RequestHandler = async (req, res) => {
+            return 'nest not found';
+        };
+        const notFound: RequestHandler = async (req, res) => {
+            return 'not found';
+        };
+
+        const route = router([path1, path2]);
+        const nestedRoute = createNamespace('/nest')(route(nestNotFound));
+        const server = serve(router([nestedRoute])(notFound));
+
+        try {
+            server.listen();
+            const address = <AddressInfo>server.address();
+
+            // GET /path1
+            const response1 = await (await fetch(`http://localhost:${address.port}/path1`)).text();
+            expect(response1).toBe('not found');
+
+            // POST /path2
+            const response2 = await (await fetch(`http://localhost:${address.port}/path2`, { method: 'POST' })).text();
+            expect(response2).toBe('not found');
+
+            // GET /nest/path1
+            const response4 = await (await fetch(`http://localhost:${address.port}/nest/path1`)).text();
+            expect(response4).toBe('get path1');
+
+            // POST /nest/path2
+            const response5 = await (await fetch(`http://localhost:${address.port}/nest/path2`, {
+                method: 'POST'
+            })).text();
+            expect(response5).toBe('post path2');
+
+            // GET /nest/not_found
+            const response6 = await (await fetch(`http://localhost:${address.port}/nest/not_found`)).text();
+            expect(response6).toBe('nest not found');
+        } finally {
+            server.close();
+        }
+    });
 });
