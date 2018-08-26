66c55c31cc1b881a6a4603ccca28f499dd329782
1gy <tosh@tosh.local>
2018-08-26 22:27:17 +0900

    Add router test

---
+++ packages/leicht/tests/router.test.ts
@@ -0,0 +1,63 @@
+import fetch from 'node-fetch';
+import { AddressInfo } from 'net';
+import { serve, RequestHandler } from '../src/serve';
+import { router, get, post, createNamespace } from '../src/router';
+
+describe('router', () => {
+    it('no error', async () => {
+        const path1 = get('/path1')(async (req, res) => {
+            return 'get path1';
+        });
+        const path2 = post('/path2')(async (req, res) => {
+            return 'post path2';
+        });
+        const notFound: RequestHandler = async (req, res) => {
+            return 'not found';
+        };
+        const route = router([path1, path2]);
+        const server = serve(route(notFound));
+
+        try {
+            server.listen();
+        } finally {
+            server.close();
+        }
+    });
+
+    it('simple routing', async () => {
+        const path1 = get('/path1')(async (req, res) => {
+            return 'get path1';
+        });
+        const path2 = post('/path2')(async (req, res) => {
+            return 'post path2';
+        });
+        const notFound: RequestHandler = async (req, res) => {
+            return 'not found';
+        };
+        const route = router([path1, path2]);
+        const server = serve(route(notFound));
+
+        try {
+            server.listen();
+            const address = <AddressInfo>server.address();
+
+            // GET /path1
+            const response1 = await (await fetch(`http://localhost:${address.port}/path1`)).text();
+            expect(response1).toBe('get path1');
+
+            // POST /path2
+            const response2 = await (await fetch(`http://localhost:${address.port}/path2`, { method: 'POST' })).text();
+            expect(response2).toBe('post path2');
+
+            // POST /path1
+            const response3 = await (await fetch(`http://localhost:${address.port}/path1`, { method: 'POST' })).text();
+            expect(response3).toBe('not found');
+
+            // POST /not_found
+            const response4 = await (await fetch(`http://localhost:${address.port}/not_found`)).text();
+            expect(response4).toBe('not found');
+        } finally {
+            server.close();
+        }
+    });
+});
